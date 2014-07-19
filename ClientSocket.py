df6012e285c143b4eb8c0972bbb460d2f5d717d8
thengoalie <ngoalie@gmail.com>
2014-07-19 15:29:00 -0500

    Sending binary data, verifying the checksum and then unpacking the data

--- ClientSocket.py
+++ ClientSocket.py
@@ -1,21 +1,36 @@
 import socket
 import sys
+import time
+
+MESSAGE_DELAY = 1000
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+data = '\x04\x0F\x0F\x00\x00\xEF'
+#data = 'aabbccdd'
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Create function get get time from
+current_milli_time = lambda: int(round(time.time() * 1000))
+timer = 0
+
 try:
     # Connect to server and send data
     sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
+    while(1):
+        if( (current_milli_time() - timer) > MESSAGE_DELAY):    
+            sock.sendall(data)
+
+            # Receive data from the server and shut down
+            #received = sock.recv(1024)
+            #print "Sent:     {}".format(data)
+            #print "Received: {}".format(received)
+            time.strftime("%S")
+            timer = current_milli_time()
+        
 finally:
     sock.close()
 
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+
--- ServerSocket.py
+++ ServerSocket.py
@@ -1,35 +1,53 @@
-import socket
 import sys
-import time
+import SocketServer
 
-MESSAGE_DELAY = 1000
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    def processPKT(self, data_to_process):
+        local_data = bytearray(data_to_process)
+        print "Throttle:",
+        print str(local_data[1])
+        print "Left/Right:",
+        print str(local_data[2])
+        print "Something else:",
+        print str(local_data[4])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def isValidPKT(self):
+        byteArr = bytearray(self.data)
+        #Check the checksum
+        checksum = 0
+        #print sys.getsizeof(byteArr)
+        for x in range(0, 6) :
+            #print x
+            if((x!=0) and (x!=5)):
+                checksum = byteArr[ x ] +  checksum
+        return( ( checksum & 0x0F ) == ( ( byteArr[5] & 0xF0 ) >> 4 ) )    
 
-# Create function get get time from
-current_milli_time = lambda: int(round(time.time() * 1000))
-timer = 0
+    def handle(self):
+        while 1:
+            self.data = self.request.recv(1024)
+            if not self.data:
+                break
+            self.data = self.data.strip()
+            #print str(self.client_address[0]) + " wrote: "
+            #print self.data
+            self.request.send(self.data.upper())
+            if self.isValidPKT( ):
+                self.processPKT( self.data )
+    
 
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
 
-    while(1)
-        if( (current_milli_time() - timer) > MESSAGE_DELAY)    
-            sock.sendall("HI" + "\n")
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
-            # Receive data from the server and shut down
-            received = sock.recv(1024)
-            print "Sent:     {}".format(data)
-            print "Received: {}".format(received)
-            time.strftime("%S")
-            timer = current_milli_time()
-        
-finally:
-    sock.close()
-
-
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
